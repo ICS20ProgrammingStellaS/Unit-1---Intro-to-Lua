624f4be6b6d0ecd6dc499349d11415da9958693e
Stella Armstrong <stella.armstrong@ocsbstudent.ca>
2019-10-02 14:57:58 -0400

    Finnish project

--- TouchAndReactStella/main.lua
+++ TouchAndReactStella/main.lua
@@ -13,7 +13,7 @@ display.setStatusBar(display.HiddenStatusBar)
 local blueButton = display.newImageRect("Images/Fast Button Inactive@2x.png", 198, 96)
 blueButton.x = display.contentWidth/2
 blueButton.y = display.contentHeight/2
-blueButton.isVisible = true 
+blueButton.isVisible = true
 
 -- create red Button, set its position and make it invisible
 local redButton = display.newImageRect("Images/Fast Button Active@2x.png", 198, 96)
@@ -29,7 +29,60 @@ textObject:setTextColor (1, 1, 0)
 textObject.isVisible = false
 
 
+local check = display.newImageRect("Images/checkmark.png", 198, 108)
+check.x = 520
+check.y = 550
+check.isVisible = false
+
+local correctSound = audio.loadSound("Sounds/Correct Answer Sound Effect.mp3")
+
 -- Function: BlueButtonListener
 -- Input: touch listener
 -- Output: none 
--- Description: when the blue button is clicked, it will make the text
+-- Description: when the blue button is clicked, it will make the textappear
+-- with the red button, and make blue button disappear.
+local function BlueButtonListener(touch)
+	if (touch.phase == "began") then 
+		blueButton.isVisible = false
+		redButton.isVisible = true
+		textObject.isVisible = true
+		check.isVisible = true
+		--play the sound on any available channel
+		local correctSoundChannel = audio.play(correctSound)
+	end
+
+	if (touch.phase == "ended") then
+		blueButton.isVisible = true
+		redButton.isVisible = false
+		textObject.isVisible = false
+		check.isVisible = false
+	end
+end
+
+-- add the respective listener to each object
+blueButton:addEventListener("touch", BlueButtonListener)
+
+-- Function: RedButtonListener
+-- Input: touch listener
+-- Output: none 
+-- Description: when the blue button is clicked, it will make the textappear
+-- with the red button, and make blue button disappear.
+local function RedButtonListener2(touch)
+	if (touch.phase == "beagn") then 
+		blueButton.isVisible = false
+		redButton.isVisible = true
+		textObject.isVisible = true
+		check.isVisible = true
+	end
+
+	if (touch.phase == "ended") then
+		blueButton.isVisible = true
+		redButton.isVisible = false
+		textObject.isVisible = false
+		check.isVisible = false
+	end
+end
+
+-- add the respective listener to each object
+redButton:addEventListener("touch", RedButtonListener2)
+
