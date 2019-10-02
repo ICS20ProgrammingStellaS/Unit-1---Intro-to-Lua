8d1bda05ddbc12e4bbc4e49a5bb557221ae9619d
Stella Armstrong <stella.armstrong@ocsbstudent.ca>
2019-10-02 14:04:26 -0400

    Added Touch And React

---
+++ TouchAndReactStella/main.lua
@@ -0,0 +1,35 @@
+-- Title: TouchAndReact
+-- Name: Stella Armstron
+-- Course: ICS2O
+-- This program does something when i click on the button. 
+
+-- set background colour
+display.setDefault ("background", 153/255, 204/255, 255/255)
+
+-- hide status bar
+display.setStatusBar(display.HiddenStatusBar)
+
+-- create blue button, set its position and make it visible
+local blueButton = display.newImageRect("Images/Fast Button Inactive@2x.png", 198, 96)
+blueButton.x = display.contentWidth/2
+blueButton.y = display.contentHeight/2
+blueButton.isVisible = true 
+
+-- create red Button, set its position and make it invisible
+local redButton = display.newImageRect("Images/Fast Button Active@2x.png", 198, 96)
+redButton.x = display.contentWidth/2
+redButton.y = display.contentHeight/2
+redButton.isVisible = false
+
+-- create text object, set its position and make it invisible
+local textObject = display.newText ("Clicked!", 0, 0, nil, 50)
+textObject.x = display.contentWidth/2
+textObject.y = display.contentHeight/3
+textObject:setTextColor (1, 1, 0)
+textObject.isVisible = false
+
+
+-- Function: BlueButtonListener
+-- Input: touch listener
+-- Output: none 
+-- Description: when the blue button is clicked, it will make the text
