b1f50d2d9a3569b5baddc260123925f1df11ed43
Stella Armstrong <stella.armstrong@ocsbstudent.ca>
2019-10-03 14:44:48 -0400

    Finnished

--- NumericTextFieldsStella/main.lua
+++ NumericTextFieldsStella/main.lua
@@ -1,7 +1,113 @@
------------------------------------------------------------------------------------------
---
--- main.lua
---
------------------------------------------------------------------------------------------
+-- Title: NumericTextFields
+-- Name: Stella Armstrong
+-- Course: ICS2O
+-- This program displays a math question and asks the user to answer in a numeric textfeild.
+-- terminal
+---------------------------------------------------------------------------------------------
 
--- Your code here
+-- hide the status bar
+display.setStatusBar(display.HiddenStatusBar)
+
+-- sets the background colour
+display.setDefault("background", 255/255, 204/255, 153/255)
+
+---------------------------------------------------------------------------------------------
+-- LOCAL VARIABLES
+---------------------------------------------------------------------------------------------
+
+-- create local variables
+local questionObject
+local correctObject
+local incorrectObject
+local numericField
+local randomNumber1
+local randomNumber2
+local userAnswer
+local correctAnswer
+local incorrectAnswer
+
+---------------------------------------------------------------------------------------------
+-- LOCAL FUNCTIONS
+---------------------------------------------------------------------------------------------
+
+local function AskQuestion()
+	-- generate 2 random numbers between a max. and a min. number
+	randomNumber1 = math.random(0, 10)
+	randomNumber2 = math.random(0, 10)
+
+	correctAnswer = randomNumber1 + randomNumber2
+
+	-- create question in text object 
+	questionObject.text = randomNumber1 .. " + " .. randomNumber2 .. " = "
+
+end
+
+local function HideCorrect()
+	correctObject.isVisible = false
+	AskQuestion()
+end
+
+local function HideIncorrect()
+	incorrectObject.isVisible = false
+	AskQuestion()
+end
+
+local function NumericFieldListener( event )
+	-- User begins editing "numericField"
+	if ( event.phase == "began" ) then
+
+		-- clear text field
+		event.target.text = ""
+
+	elseif event.phase == "submitted" then
+
+		-- when the answer is submitted (enter key is pressed) set user input to user's answer
+		userAnswer = tonumber(event.target.text)
+
+		-- if the user answer andthe correct answer are the same:
+		if (userAnswer == correctAnswer) then 
+			correctObject.isVisible = true
+			timer.performWithDelay(2000, HideCorrect)
+		else 
+			incorrectObject.isVisible = true
+			timer.performWithDelay(2000, HideIncorrect)
+
+		end
+		-- clear text field
+		event.target.text = ""
+
+	end
+end
+
+---------------------------------------------------------------------------------------------
+-- OBJECT CREATION 
+---------------------------------------------------------------------------------------------
+
+-- displays a question and sets the colour
+questionObject = display.newText( "", display.contentWidth/3, display.contentHeight/2, nil, 50)
+questionObject:setTextColor(155/255, 42/255, 198/255)
+
+-- create the correct text object and make it invisible
+correctObject = display.newText( "Correct!", display.contentWidth/2, display.contentHeight*2/3, nil, 50)
+correctObject:setTextColor(155/255, 42/255, 198/255)
+correctObject.isVisible = false
+
+-- create the correct text object and make it invisible
+incorrectObject = display.newText( "Incorrect!", display.contentWidth/2, display.contentHeight*2/3, nil, 50)
+incorrectObject:setTextColor(155/255, 42/255, 198/255)
+incorrectObject.isVisible = false
+
+
+-- Create numeric field
+numericField = native.newTextField( display.contentWidth/2, display.contentHeight/2, 150, 80)
+numericField.inputType = "number"
+
+-- add the event listener for the numeric field
+numericField:addEventListener( "userInput", NumericFieldListener)
+
+--------------------------------------------------------------------------------------------
+-- FUNCTION CALLS
+--------------------------------------------------------------------------------------------
+
+-- call the function to ask the question
+AskQuestion()
