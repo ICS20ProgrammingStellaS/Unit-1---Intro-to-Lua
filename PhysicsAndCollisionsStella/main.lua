9ee0795c25833341df24e3a207190f4c07b48423
Stella Armstrong <stella.armstrong@ocsbstudent.ca>
2019-10-17 14:29:08 -0400

    added

--- PhysicsAndCollisionsStella/main.lua
+++ PhysicsAndCollisionsStella/main.lua
@@ -1,7 +1,13 @@
------------------------------------------------------------------------------------------
---
--- main.lua
---
------------------------------------------------------------------------------------------
+-- Title: Physics and Collision
+-- Name: Stella Armstrong
+-- Course: ICS20
+-- This program displays 4 different balls/objects that falls from different
+--  sides of the screen onto a slide and diving board. I added a diving wall 
+-- on the sides of the screen to stop the balls/objects from rolling.
+---------------------------------------------------------------------------------------------
 
--- Your code here
+-- hide the status bar
+display.setStatusBar(display.HiddenStatusBar)
+
+-- load physics
+local physics = require("physics")
