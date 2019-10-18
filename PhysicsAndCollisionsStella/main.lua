d1fdac6b072af114d11266f321ffaf9c520c6b6f
Stella Armstrong <stella.armstrong@ocsbstudent.ca>
2019-10-17 21:10:13 -0400

    added stuff

--- PhysicsAndCollisionsStella/main.lua
+++ PhysicsAndCollisionsStella/main.lua
@@ -9,5 +9,27 @@
 -- hide the status bar
 display.setStatusBar(display.HiddenStatusBar)
 
+-- add background image with width and height 
+local backgroundImage = display.newImageRect("Images/Pool.jpg", 2048, 1536)
+backgroundImage.x = 500
+backgroundImage.y = 0
+
 -- load physics
 local physics = require("physics")
+
+-- start physics
+physics.start()
+----------------------------------------------------------------------------------------------
+-- OBJECTS
+----------------------------------------------------------------------------------------------
+local divingBoard = display.newImage("Physics/divingBoard.png", 0, 0)
+	divingBoard.x = 500
+	divingBoard.y = 700
+
+	-- change the width to be the same as the screen
+	divingBoard.width = display.contentWidth
+
+	-- add the physics
+	physics.addBody(divingBoard, "static", {friction=0.5, bounce=0.3})
+
+	------------------------------------------------------------------------------------
